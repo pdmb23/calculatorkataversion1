418c18e6f56bed7d388ee3dc80072b15a1c14e7f
Princess Diana B. Ongchangco <pdmb23@yahoo.com>
2015-04-28 17:12:01 +0800

    check-in to github online
    check-in to github online

---
+++ CalculatorKata/CalculatorApp.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorKata
+{
+    public class CalculatorApp
+    {
+        #region Fields
+        List<string> inputCollection;
+        List<double> numberCollection;
+        Queue<string> decimalNumberCollection;
+        Queue<string> operatorCollection;
+        List<string> operationList;
+        double runningTotal;
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructor
+
+        public CalculatorApp()
+        {
+            this.inputCollection = new List<string>();
+            this.numberCollection = new List<double>();
+            this.operatorCollection = new Queue<string>();
+            this.decimalNumberCollection = new Queue<string>();
+            this.operationList = new List<string>() { "+", "-", "*", "/" };
+            this.runningTotal = 0;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Input(string token)
+        {
+            this.inputCollection.Add(token);
+        }
+
+        public double GetAnswer()
+        {
+            this.LexicalAnalysis();
+            var operatorValue = string.Empty;
+            double num = 0;
+            for (int i = 0; i < this.numberCollection.Count; i++)
+            {
+                num = this.numberCollection[i];
+                if (i == 0)
+                {
+                    this.runningTotal = num;
+                    continue;
+                }
+                else
+                {
+                    if (this.operatorCollection.Count >= 1)
+                    {
+                        operatorValue = this.operatorCollection.Dequeue();
+                    }
+                    switch (operatorValue)
+                    {
+                        case "+": this.runningTotal += num;
+                            break;
+                        case "-": this.runningTotal -= num;
+                            break;
+                        case "*": this.runningTotal *= num;
+                            break;
+                        case "/": this.runningTotal /= num;
+                            break;
+                    }
+                }
+            }
+            return Math.Round(this.runningTotal, 1);
+        }
+
+        private bool IsNumber(string entry)
+        {
+            int numberValue;
+            if (Int32.TryParse(entry, out numberValue))
+            {                
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool IsDecimalSeparator(string entry)
+        {
+            if (entry == ".")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool IsOperator(string entry)
+        {
+            if (this.operationList.Contains(entry))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void LexicalAnalysis()
+        {
+            int index = 0;
+            while (index < this.inputCollection.Count)
+            {
+                var currentToken = this.GetInput(index);
+                if (IsNumber(currentToken))
+                {
+                    string decimalNumberValue = this.DecimalNumberParse(ref index, ref currentToken);
+                    this.numberCollection.Add(Convert.ToDouble(decimalNumberValue));
+                }                
+                else if (IsOperator(currentToken))
+                {
+                    this.operatorCollection.Enqueue(currentToken);
+                    index++;
+                }
+            }
+        }
+
+        private string DecimalNumberParse(ref int index, ref string currentToken)
+        {
+            string decimalNumberValue = string.Empty;
+            this.IntegerNumberParse(ref index, ref currentToken);
+            var lookAhead = this.GetInput(index);
+            if (IsDecimalSeparator(lookAhead))
+            {
+                this.decimalNumberCollection.Enqueue(lookAhead);
+                index = index + 1;
+                currentToken = this.GetInput(index);
+                this.IntegerNumberParse(ref index, ref currentToken);
+            }            
+            while (this.decimalNumberCollection.Count > 0)
+            {
+                decimalNumberValue = decimalNumberValue + this.decimalNumberCollection.Dequeue();
+            }
+            return decimalNumberValue;
+        }
+
+        private void IntegerNumberParse(ref int index, ref string currentToken)
+        {
+            while (IsNumber(currentToken))
+            {
+                this.decimalNumberCollection.Enqueue(currentToken);
+                index = index + 1;
+                currentToken = this.GetInput(index);
+            }
+        }
+
+        private string GetInput(int index)
+        {
+            if (index < this.inputCollection.Count)
+            {
+                return this.inputCollection[index];
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ CalculatorTest/CalculatorAppTest.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using CalculatorKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class CalculatorAppTest
+    {
+        [TestMethod]
+        public void AddTwoNumbers_Should_Be_Sum()
+        {
+            //arrange
+            CalculatorApp calculatorApp = new CalculatorApp();
+            //act
+            calculatorApp.Input("2");
+            calculatorApp.Input("+");
+            calculatorApp.Input("3");            
+            var actualResult = calculatorApp.GetAnswer();
+            //assert
+            var expectedResult = 5;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void SubtractTwoNumbers_Should_Be_Difference()
+        {
+            //arrange
+            CalculatorApp calculatorApp = new CalculatorApp();
+            //act
+            calculatorApp.Input("5");
+            calculatorApp.Input("-");
+            calculatorApp.Input("3");
+            var actualResult = calculatorApp.GetAnswer();
+            //assert
+            var expectedResult = 2;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void MultiplyTwoNumbers_Should_Be_Product()
+        {
+            //arrange
+            CalculatorApp calculatorApp = new CalculatorApp();
+            //act
+            calculatorApp.Input("5");
+            calculatorApp.Input("*");
+            calculatorApp.Input("3");
+            var actualResult = calculatorApp.GetAnswer();
+            //assert
+            var expectedResult = 15;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void DivideTwoNumbers_Should_Be_Quotient()
+        {
+            //arrange
+            CalculatorApp calculatorApp = new CalculatorApp();
+            //act
+            calculatorApp.Input("6");
+            calculatorApp.Input("/");
+            calculatorApp.Input("3");
+            var actualResult = calculatorApp.GetAnswer();
+            //assert
+            var expectedResult = 2;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ComputeThreeNumbers_Should_Be_Sum_And_Product()
+        {
+            //arrange
+            CalculatorApp calculatorApp = new CalculatorApp();
+            //act
+            calculatorApp.Input("2");
+            calculatorApp.Input("+");
+            calculatorApp.Input("3");
+            calculatorApp.Input("*");
+            calculatorApp.Input("4");
+            var actualResult = calculatorApp.GetAnswer();
+            //assert
+            var expectedResult = 20;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ComputeFourNumbers_Should_Be_Sum_And_Product_And_Quotient()
+        {
+            //arrange
+            CalculatorApp calculatorApp = new CalculatorApp();
+            //act
+            calculatorApp.Input("2");
+            calculatorApp.Input("+");
+            calculatorApp.Input("3");
+            calculatorApp.Input("*");
+            calculatorApp.Input("4");
+            calculatorApp.Input("/");
+            calculatorApp.Input("2");
+            var actualResult = calculatorApp.GetAnswer();
+            //assert
+            var expectedResult = 10;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ComputeWithDecimalNumbers_Should_Be_Sum_And_Product_And_Quotient()
+        {
+            //arrange
+            CalculatorApp calculatorApp = new CalculatorApp();
+            //act
+            calculatorApp.Input("2");
+            calculatorApp.Input(".");
+            calculatorApp.Input("5");
+            calculatorApp.Input("+");
+            calculatorApp.Input("3");
+            calculatorApp.Input(".");
+            calculatorApp.Input("5");
+            calculatorApp.Input("*");
+            calculatorApp.Input("10");
+            calculatorApp.Input(".");
+            calculatorApp.Input("1");
+            calculatorApp.Input("/");
+            calculatorApp.Input("2");
+            var actualResult = calculatorApp.GetAnswer();
+            //assert
+            double expectedResult = 30.3;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersWithTwoDigitsEach_Should_Be_Sum()
+        {
+            //arrange
+            CalculatorApp calculatorApp = new CalculatorApp();
+            //act
+            calculatorApp.Input("2");
+            calculatorApp.Input("4");
+            calculatorApp.Input("+");
+            calculatorApp.Input("3");
+            calculatorApp.Input("5");
+            var actualResult = calculatorApp.GetAnswer();
+            //assert
+            var expectedResult = 59;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ComputeWithMoreThanOneDigitAndDecimalNumbers_Should_Be_Sum_And_Product_And_Quotient()
+        {
+            //arrange
+            CalculatorApp calculatorApp = new CalculatorApp();
+            //act
+            calculatorApp.Input("2");
+            calculatorApp.Input("4");
+            calculatorApp.Input(".");
+            calculatorApp.Input("5");
+            calculatorApp.Input("9");
+            calculatorApp.Input("+");
+            calculatorApp.Input("3");
+            calculatorApp.Input("8");
+            calculatorApp.Input(".");
+            calculatorApp.Input("5"); 
+            calculatorApp.Input("4");
+            calculatorApp.Input("*");
+            calculatorApp.Input("10");
+            calculatorApp.Input(".");
+            calculatorApp.Input("1");
+            calculatorApp.Input("/");
+            calculatorApp.Input("2");
+            var actualResult = calculatorApp.GetAnswer();
+            //assert
+            double expectedResult = 318.8;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
